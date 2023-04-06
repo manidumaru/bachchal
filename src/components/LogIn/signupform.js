2e13ca6f28fd1910017d175ee185736020e9601a
Nirbhay Adhikari <fearalert1@gmail.com>
2023-04-06 17:23:54 +0545

    basic authentication for signup done

--- src/components/LogIn/signupform.js
+++ src/components/LogIn/signupform.js
@@ -6,8 +6,37 @@ import logo_google from "../../assets/google_logo.png";
 import username from "../../assets/username.png";
 import Modal from "../UI/Modal";
 import "./signupform.css";
+import axios from "axios";
+import { useState } from "react";
 
 function SignUpForm(props) {
+  const [errorInfo, setErrorInfo] = useState(null);
+  const [responseInfo, setResponseInfo] = useState(null);
+
+  const signUP = async (event) => {
+    event.preventDefault();
+    await axios
+      .post("http://127.0.0.1:8000/basicauth/signup/", {
+        username: userDetails.userName,
+        email: userDetails.userEmail,
+        password: userDetails.userPassword,
+      })
+      .then(function (response) {
+        console.log(response);
+        setResponseInfo(response);
+      })
+      .catch(function (error) {
+        console.log(error.response.data.error);
+        setErrorInfo(error.response.data.error);
+      });
+  };
+
+  const [userDetails, setUserDetails] = useState({
+    userEmail: "",
+    userPassword: "",
+    userName: "",
+  });
+
   return (
     <Modal onClick={props.onclose}>
       <div className="sign_up_main">
@@ -15,12 +44,20 @@ function SignUpForm(props) {
           <img src={greenlogo} alt="Logo" height="50px"></img>
         </div>
         <div className="sign_up_form">
-          <form>
+          <form onSubmit={signUP}>
             <div className="form_group">
               <input
                 type="username"
+                name="username"
                 id="username"
                 placeholder="Username"
+                value={userDetails.userName}
+                onChange={(e) =>
+                  setUserDetails({
+                    ...userDetails,
+                    userName: e.target.value,
+                  })
+                }
               ></input>
               <div className="icon">
                 <img
@@ -33,7 +70,18 @@ function SignUpForm(props) {
               </div>
             </div>
             <div className="form_group">
-              <input type="email" id="email" placeholder="Email"></input>
+              <input
+                type="email"
+                id="email"
+                placeholder="Email"
+                value={userDetails.userEmail}
+                onChange={(e) =>
+                  setUserDetails({
+                    ...userDetails,
+                    userEmail: e.target.value,
+                  })
+                }
+              ></input>
               <div className="icon">
                 <img
                   className="signup_icon2"
@@ -49,6 +97,14 @@ function SignUpForm(props) {
                 type="password"
                 id="password"
                 placeholder="Password"
+                name="password"
+                value={userDetails.userPassword}
+                onChange={(e) =>
+                  setUserDetails({
+                    ...userDetails,
+                    userPassword: e.target.value,
+                  })
+                }
               ></input>
               <div className="icon">
                 <img
